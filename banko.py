b681aa1c159f143580b961b5ad304303949fb45a
Mathias Rav <m@git.strova.dk>
2020-12-05 22:23:39 +0100

    Fix generation

--- banko.py
+++ banko.py
@@ -1,4 +1,5 @@
 import argparse
+import math
 from typing import List
 
 import numpy as np
@@ -10,43 +11,63 @@ parser.add_argument("n", type=int)
 
 
 PREAMBLE = r"""
-\documentclass{memoir}
-\newcommand{\banko}[1]{\framebox[1cm][c]{\rule[-3mm]{0pt}{\dimexpr 1cm-2\fboxsep}#1}}
+\documentclass[extrafontsizes,17pt,oneside]{memoir}
+\newcommand{\plade}[4]{%
+    \par\vspace{4ex}%
+    \noindent{\small ID: #1\\}%
+    #2\\[-0.2ex]#3\\[-0.2ex]#4\\[-0.2ex]%
+}
+\newcommand{\tal}[1]{%
+    \framebox[6ex][c]{\rule[-2ex]{0pt}{\dimexpr 6ex-2\fboxsep}#1}%
+}
+\newcommand{\tom}{\tal{}}
 \begin{document}
 """.strip()
 
-# \vspace{1cm}
-# \noindent
-# {\small ID: 123\\}
-# \banko{42}%
-# \banko{43}%
-# \\[-1pt]%
-# \banko{44}%
-# \banko{45}%
-# \\[-1pt]%
-#
-# \vspace{1cm}
-# \noindent
-# {\small ID: 123\\}
-# \banko{42}%
-# \banko{43}%
-# \\[-1pt]%
-# \banko{44}%
-# \banko{45}%
-# \\[-1pt]%
-#
-# \end{document}
+
+def comb(N: int, k: int) -> int:
+    return math.factorial(N) // (math.factorial(k) * math.factorial(N - k))
 
 
-def plade(seed: int) -> List[int]:
+def plade(seed: int) -> List[List[int]]:
     rng = np.random.RandomState(seed)
     rows = 3
     cols = 9
-    whichones = np.sort(rng.choice(rows * cols, (15,), False))
-    col = whichones // rows
-    result: List[int] = []
+    # Need to place 5 numbers in each row, so n=15 in total.
+    numbers_in_row = 5
+    n = numbers_in_row * rows
+    # Each column needs at least 1 number,
+    # so there are only x=n-9 extra numbers that we need to place.
+    x = n - cols
+    # Need to pick a,b>=0, a+b<=9 so that (9-a-b) + 2a + 3b = n.
+    # 9 + a + 2b = n
+    # a = n - 9 - 2b
+    # b >= 0
+    # -2b <= 0
+    # 0 <= a <= n - 9
+    # 0 <= n - 9 - 2b
+    # 2b <= n - 9
+    # b <= (n - 9) // 2
+    # C(9,b) * C(9-b,a)
+    ab = [(x - 2 * b, b) for b in range(min(x // 2, cols) + 1)]
+    assert all(2 * a + 3 * b + (cols - a - b) == n for a, b in ab)
+    weights = np.asarray([comb(cols, b) * comb(cols - b, a) for a, b in ab])
+    colcounts = np.ones(cols, dtype=np.intp)
+    a, b = ab[rng.choice(len(ab), 1, True, weights / weights.sum())[0]]
+    colcounts[rng.choice(cols, a, False)] = 2
+    colcounts[rng.choice(np.equal(colcounts, 1).nonzero()[0], b, False)] = 3
+    assert colcounts.sum() == n, (colcounts, colcounts.sum(), n)
+    rowcounts = np.zeros(rows)
+    mask = np.zeros((cols, rows), dtype=np.bool)
+    for i in sorted(range(cols), key=lambda i: -colcounts[i]):
+        free = np.less(rowcounts, numbers_in_row).nonzero()[0]
+        assert len(free) >= colcounts[i], (free, colcounts[i])
+        r = rng.choice(free, colcounts[i], False)
+        rowcounts[r] += 1
+        mask[i][r] = True
+    nums = np.zeros((cols, rows), dtype=np.intp)
     for i in range(cols):
-        count = np.sum(col == i)
+        count = np.sum(mask[i])
         if not count:
             continue
         lo = i * 10
@@ -55,14 +76,14 @@ def plade(seed: int) -> List[int]:
             lo = 1
         if i == cols - 1:
             hi += 1
-        result += sorted(rng.choice(np.arange(lo, hi), (count,), False).tolist())
-    assert result == sorted(result), result
-    assert len(result) == len(set(result)), result
-    nums = np.zeros(rows * cols)
-    assert len(whichones) == len(result)
-    for i, r in zip(whichones, result):
-        nums[i] = r
-    return np.transpose(nums.reshape((cols, rows))).tolist()
+        nums[i][mask[i]] = np.sort(rng.choice(np.arange(lo, hi), (count,), False))
+    return np.transpose(nums).tolist()
+
+
+def print_ascii_plade(p: List[List[int]]) -> None:
+    print("_" * (5 * 9 + 1))
+    for r in p:
+        print("|%s|" % "|".join(str(v or "").center(4, "_") for v in r))
 
 
 def main() -> None:
@@ -72,17 +93,21 @@ def main() -> None:
     else:
         rng = np.random.RandomState(args.seed)
         seeds = sorted(rng.choice(1000, args.n, False).tolist())
-    print(PREAMBLE)
-    for i in seeds:
-        print("")
-        print(r"\vspace{1cm}")
-        print(r"\noindent")
-        print(r"{\small ID: %s\\}" % i)
-        for row in plade(i):
-            for v in row:
-                print(r"\banko{%s}%%" % (int(v) or ""))
-            print(r"\\[-1pt]%")
-    print(r"\end{document}")
+    with open("banko.tex", "w") as fp:
+        print(PREAMBLE, file=fp)
+        for i, seed in enumerate(seeds):
+            p = plade(seed)
+            # print_ascii_plade(p)
+            print(r"\plade{%s}%%" % seed, file=fp)
+            for row in p:
+                print(
+                    "{%s}%%"
+                    % "".join(r"\tom" if v == 0 else r"\tal{%s}" % v for v in row),
+                    file=fp,
+                )
+            if (i + 1) % 3 == 0:
+                print(r"\newpage", file=fp)
+        print(r"\end{document}", file=fp)
 
 
 if __name__ == "__main__":
